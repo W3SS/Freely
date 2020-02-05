026d73529fd7ab93c4af015b116b077d58b09a80
Wendel G. Santana <gswendelw3ss@gmail.com>
2020-02-05 14:30:13 -0300

    Initial Commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 80px;
+  padding: 0 30px;
+  background: #D53D70;
+  color: #FFF;
+
+  display: flex;
+  align-items: center;
+`;
